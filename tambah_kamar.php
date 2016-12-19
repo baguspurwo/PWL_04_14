93f796d18b1dac0ce8e3ac666970fb4d0c337369
baguspurwo <bagus8092@students.amikom.ac.id>
2016-12-19 06:45:27 -0800

    berubah

---
+++ tambah_kamar.php
@@ -0,0 +1,257 @@
+<?php 
+session_start();
+if (empty($_SESSION['username'])){
+	echo "<script>alert('Anda belum mempunyai hak akses.'); window.location = '../index.html'</script>";	
+} else {
+	include "../koneksi.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Aplikasi Penggajian Karyawan">
+    <meta name="author" content="Hakko Bio Richard">
+
+    <title>Welcome Admin SI HOTEL</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <!-- Page Specific CSS -->
+    <link rel="stylesheet" href="http://cdn.oesmith.co.uk/morris-0.4.3.min.css">
+    
+    <script type="text/javascript">
+// 1 detik = 1000
+window.setTimeout("waktu()",1000);  
+function waktu() {   
+	var tanggal = new Date();  
+	setTimeout("waktu()",1000);  
+	document.getElementById("output").innerHTML = tanggal.getHours()+":"+tanggal.getMinutes()+":"+tanggal.getSeconds();
+}
+</script>
+<script language="JavaScript">
+var tanggallengkap = new String();
+var namahari = ("Minggu Senin Selasa Rabu Kamis Jumat Sabtu");
+namahari = namahari.split(" ");
+var namabulan = ("Januari Februari Maret April Mei Juni Juli Agustus September Oktober November Desember");
+namabulan = namabulan.split(" ");
+var tgl = new Date();
+var hari = tgl.getDay();
+var tanggal = tgl.getDate();
+var bulan = tgl.getMonth();
+var tahun = tgl.getFullYear();
+tanggallengkap = namahari[hari] + ", " +tanggal + " " + namabulan[bulan] + " " + tahun;
+
+	var popupWindow = null;
+	function centeredPopup(url,winName,w,h,scroll){
+	LeftPosition = (screen.width) ? (screen.width-w)/2 : 0;
+	TopPosition = (screen.height) ? (screen.height-h)/2 : 0;
+	settings ='height='+h+',width='+w+',top='+TopPosition+',left='+LeftPosition+',scrollbars='+scroll+',resizable'
+	popupWindow = window.open(url,winName,settings)
+}
+</script>
+    
+  </head>
+  <body>
+    <div id="wrapper">
+
+      <!-- Sidebar -->
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Admin SI HOTEL</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav side-nav">
+            <li class="active"><a href="data_kamar.php"><i class="fa fa-table"></i> Data Kamar</a></li>
+            <li><a href="data_customer.php"><i class="fa fa-table"></i> Data Customer</a></li>
+            <li><a href="data_typekamar.php"><i class="fa fa-table"></i> Data Type Kamar</a></li>
+            <li><a href="#"><i class="fa fa-edit"></i> Berita</a></li>
+            </li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right navbar-user">
+            <!--<li class="dropdown messages-dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> Messages <span class="badge">7</span> <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li class="dropdown-header">7 New Messages</li>
+                <li class="message-preview">
+                  <a href="#">
+                    <span class="avatar"><img src="http://placehold.it/50x50"></span>
+                    <span class="name">John Smith:</span>
+                    <span class="message">Hey there, I wanted to ask you something...</span>
+                    <span class="time"><i class="fa fa-clock-o"></i> 4:34 PM</span>
+                  </a>
+                </li>
+                <li class="divider"></li>
+                <li class="message-preview">
+                  <a href="#">
+                    <span class="avatar"><img src="http://placehold.it/50x50"></span>
+                    <span class="name">John Smith:</span>
+                    <span class="message">Hey there, I wanted to ask you something...</span>
+                    <span class="time"><i class="fa fa-clock-o"></i> 4:34 PM</span>
+                  </a>
+                </li>
+                <li class="divider"></li>
+                <li class="message-preview">
+                  <a href="#">
+                    <span class="avatar"><img src="http://placehold.it/50x50"></span>
+                    <span class="name">John Smith:</span>
+                    <span class="message">Hey there, I wanted to ask you something...</span>
+                    <span class="time"><i class="fa fa-clock-o"></i> 4:34 PM</span>
+                  </a>
+                </li>
+                <li class="divider"></li>
+                <li><a href="#">View Inbox <span class="badge">7</span></a></li>
+              </ul>
+            </li>-->
+            <!--<li class="dropdown alerts-dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bell"></i> Alerts <span class="badge">3</span> <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Default <span class="label label-default">Default</span></a></li>
+                <li><a href="#">Primary <span class="label label-primary">Primary</span></a></li>
+                <li><a href="#">Success <span class="label label-success">Success</span></a></li>
+                <li><a href="#">Info <span class="label label-info">Info</span></a></li>
+                <li><a href="#">Warning <span class="label label-warning">Warning</span></a></li>
+                <li><a href="#">Danger <span class="label label-danger">Danger</span></a></li>
+                <li class="divider"></li>
+                <li><a href="#">View All</a></li>
+              </ul>
+            </li>-->
+            <li class="dropdown user-dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>
+              <?php
+              echo $_SESSION['username'];
+               ?>
+              <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#"><i class="fa fa-user"></i> Profile</a></li>
+                <li><a href="#"><i class="fa fa-envelope"></i> Inbox <span class="badge">7</span></a></li>
+                <li><a href="#"><i class="fa fa-gear"></i> Settings</a></li>
+                <li class="divider"></li>
+                <li><a href="../logout.php" onclick="return confirm('Apakah anda akan keluar?');"><i class="fa fa-power-off"></i> Log Out</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </nav>
+     <?php
+$timeout = 10; // Set timeout minutes
+$logout_redirect_url = "../index.html"; // Set logout URL
+
+$timeout = $timeout * 60; // Converts minutes to seconds
+if (isset($_SESSION['start_time'])) {
+    $elapsed_time = time() - $_SESSION['start_time'];
+    if ($elapsed_time >= $timeout) {
+        session_destroy();
+        echo "<script>alert('Session Anda Telah Habis!'); window.location = '$logout_redirect_url'</script>";
+    }
+}
+$_SESSION['start_time'] = time();
+?>
+<?php } ?>
+      <div id="page-wrapper">
+
+        <div class="row">
+          <div class="col-lg-12">
+         
+            <ol class="breadcrumb">
+              <li class="active"><i class="fa fa-table"></i> Tambah Data Kamar</li>
+            </ol>
+            <table width="900">
+            <tr>
+            <td width="250"><div class="Tanggal"><h4><script language="JavaScript">document.write(tanggallengkap);</script></div></h4></td> 
+            <td align="left" width="30"> - </td>
+            <td align="left" width="620"> <h4><div id="output" class="jam" ></div></h4></td>
+            </tr>
+            </table>
+            <br />
+            <div class="alert alert-success alert-dismissable">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+             ID Ruang auto number jadi tidak perlu di isi, abaikan saja..
+          </div>
+        </div><!-- /.row -->
+<!--
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-bar-chart-o"></i> Traffic Statistics: October 1, 2013 - October 31, 2013</h3>
+              </div>
+              <div class="panel-body">
+                <div id="morris-chart-area"></div>
+              </div>
+            </div>
+          </div>
+        </div>-->
+        <!-- /.row -->
+        <div class="col-lg-12">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-user"></i> Tambah Data Kamar </h3>
+              </div>
+              <div class="panel-body">
+                 <div class="table-responsive">
+                  
+    <form action="insert.php" method="post">
+    <table class="table table-condensed">
+    <tr>
+        <td><label for="kary_id">ID Ruang</label></td>
+        <td><input name="kary_id" type="text" class="form-control" id="kary_id" placeholder="ID Ruang" readonly/></td>
+      </tr>
+      <tr>
+        <td><label for="kode_kar">ID Type</label></td>
+        <td><input name="kode_kar" type="option" class="form-control" id="kode_kar" placeholder="ID Type" required/></td>
+      </tr>
+      <tr>
+        <td><label for="nama_kar">Nama Ruang</label></td>
+        <td><input name="nama_kar" type="text" class="form-control" id="nama_kar" placeholder="Nama Ruang" required/></td>
+      </tr>
+      <tr>
+        <td><label for="alamat_kar">Status</label></td>
+        <td><input name="alamat_kar" type="text" class="form-control" id="alamat_kar" placeholder="Status" required/></td>
+      </tr>
+      
+      <tr>
+        <td><input type="submit" value="Simpan Data"  class="btn btn-sm btn-primary"/>&nbsp;<a href="index.php" class="btn btn-sm btn-primary">Kembali</a></td>
+        </tr>
+    </table>
+    </form>
+                   </div>
+                <div class="text-right">
+                  <a href="#"  data-toggle="tooltip" class="tip-bottom" data-original-title="Tooltip Dibawah">View All Transactions <i class="fa fa-arrow-circle-right"></i></a>
+              
+                </div>
+              </div> 
+            </div>
+          </div>
+        </div><!-- /.row --> 
+      </div><!-- /#page-wrapper -->
+
+    </div><!-- /#wrapper -->
+
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+    <!-- Page Specific Plugins -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
+    <script src="js/morris/chart-data-morris.js"></script>
+    <script src="js/tablesorter/jquery.tablesorter.js"></script>
+    <script src="js/tablesorter/tables.js"></script>
+
+  </body>
+</html>

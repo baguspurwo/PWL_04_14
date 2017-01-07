6f0e4b2d6da0aec7306a1aa64c985008f36ad460
HendyDT <hendy8105@students.amikom.ac.id>
2017-01-08 00:45:13 +0700

    update code

--- Admin/index.php
+++ Admin/index.php
@@ -5,13 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Halaman Login</title>
-
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
   </head>
   <body>
     <div class="col-md-4 col-md-offset-4 form-login">
-
     <?php
     /* handle error */
     if (isset($_GET['error'])) : ?>
@@ -22,24 +20,20 @@
           <strong>Warning!</strong> <?=base64_decode($_GET['error']);?>
         </div>
     <?php endif;?>
-
         <div class="transparan">
         <div class="outter-form-login">
         <div class="logo-login">
             <em class="glyphicon glyphicon-user"></em>
         </div>
             <form action="check-login.php" class="inner-login" method="post">
-            <h3 class="text-center title-login">Login Member</h3>
+            <h3 class="text-center title-login">Login Admin</h3>
                 <div class="form-group">
                     <input type="text" class="form-control" name="username" placeholder="Username">
                 </div>
-
                 <div class="form-group">
                     <input type="password" class="form-control" name="password" placeholder="Password">
                 </div>
-
                 <input type="submit" class="btn btn-block btn-custom-green" value="LOGIN" />
-
                 <div class="text-center forget">
                     <p>Forgot Password ?</p>
                 </div>
@@ -47,7 +41,6 @@
             </div>
         </div>
     </div>
-
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
   </body>
